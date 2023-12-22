5d3660453825a54df61bd03760346fc11c031e00
Shariq <shariqnadeem21@hotmail.com>
2023-12-23 00:19:18 +0800

    Added a navbar and initialized server to render homepage

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//take user to the home page 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+  
